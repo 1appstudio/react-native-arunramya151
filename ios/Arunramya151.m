0d741a836aa5f86e1520c25ae367b34f08532f73
appstudio@icloud.com <jk@JKs-MacBook-Pro.local>
2022-03-27 16:33:49 +0530

    test

--- ios/Arunramya151.m
+++ ios/Arunramya151.m
@@ -1,19 +1,26 @@
-#import "Arunramya151.h"
+//#import "Arunramya151.h"
+//
+//@implementation Arunramya151
+//
+//RCT_EXPORT_MODULE()
+//
+//// Example method
+//// See // https://reactnative.dev/docs/native-modules-ios
+//RCT_REMAP_METHOD(multiply,
+//                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
+//                 withResolver:(RCTPromiseResolveBlock)resolve
+//                 withRejecter:(RCTPromiseRejectBlock)reject)
+//{
+//  NSNumber *result = @([a floatValue] * [b floatValue]);
+//
+//  resolve(result);
+//}
+//
+//@end
+#import <React/RCTBridgeModule.h>
 
-@implementation Arunramya151
+@interface RCT_EXTERN_MODULE(Arunramya151, NSObject)
 
-RCT_EXPORT_MODULE()
-
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
-  NSNumber *result = @([a floatValue] * [b floatValue]);
-
-  resolve(result);
-}
+RCT_EXTERN_METHOD(start:(NSDictionary *)paymentDetails withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
 
 @end
